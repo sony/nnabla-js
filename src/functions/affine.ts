7ddad207f61d8d2718d6d5afa8344450dfd2169c
s-takuseno <Takuma.Seno@sony.com>
2021-11-27 23:46:53 +0900

    Implement affine layer

---
+++ src/functions/affine.ts
@@ -0,0 +1,104 @@
+import { GPU, IKernelRunShortcut } from 'gpu.js';
+import { IAffineParameter } from '../nnabla_pb';
+import FunctionImpl from './base';
+import Variable from '../variable';
+import { getOrThrow } from '../utils';
+
+export default class Affine implements FunctionImpl {
+  param: IAffineParameter;
+
+  gpu: GPU;
+
+  matmulKernel: IKernelRunShortcut | undefined;
+
+  biasKernel: IKernelRunShortcut | undefined;
+
+  iColSize: number;
+
+  iRowSize: number;
+
+  wColSize: number;
+
+  wRowSize: number;
+
+  oColSize: number;
+
+  oRowSize: number;
+
+  constructor(param: IAffineParameter) {
+    this.param = param;
+    this.gpu = new GPU();
+    this.matmulKernel = undefined;
+    this.biasKernel = undefined;
+    this.iColSize = 1;
+    this.iRowSize = 1;
+    this.wColSize = 1;
+    this.wRowSize = 1;
+    this.oColSize = 1;
+    this.oRowSize = 1;
+  }
+
+  setup(inputs: Variable[], outputs: Variable[]): void {
+    const baseAxis = getOrThrow<number>(this.param.baseAxis as number);
+    this.iColSize = inputs[0].shape[baseAxis];
+    this.iRowSize = inputs[0].size() / this.iColSize;
+    [this.wRowSize] = inputs[1].shape;
+    this.wColSize = inputs[1].size() / this.wRowSize;
+    this.oRowSize = this.iRowSize;
+    this.oColSize = this.wColSize;
+
+    this.matmulKernel = this.gpu
+      .createKernel(function (
+        x: number[],
+        w: number[],
+        xColSize: number,
+        wColSize: number,
+      ): number {
+        const xRow = Math.floor(this.thread.x / wColSize);
+        const wCol = this.thread.x % wColSize;
+        let output = 0.0;
+        for (let i = 0; i < xColSize; i += 1) {
+          output += x[xRow * xColSize + i] * w[i * wColSize + wCol];
+        }
+        return output;
+      })
+      .setOutput([outputs[0].size()]);
+
+    if (inputs.length === 3) {
+      this.biasKernel = this.gpu
+        .createKernel(function (x: number[], b: number[], oColSize: number): number {
+          const col = this.thread.x % oColSize;
+          return x[this.thread.x] + b[col];
+        })
+        .setOutput([outputs[0].size()]);
+    }
+  }
+
+  static validate(inputs: Variable[], outputs: Variable[]): void {
+    if (inputs.length !== 2 && inputs.length !== 3) {
+      throw Error(`invalid input length: ${inputs.length}`);
+    }
+    if (outputs.length !== 1) {
+      throw Error(`invalid output length: ${outputs.length}`);
+    }
+  }
+
+  forward(inputs: Variable[], outputs: Variable[]): void {
+    if (this.matmulKernel === undefined) {
+      throw Error('call setup first.');
+    }
+    Affine.validate(inputs, outputs);
+
+    let output = this.matmulKernel(
+      inputs[0].data,
+      inputs[1].data,
+      this.iColSize,
+      this.wColSize,
+    ) as number[];
+
+    if (this.biasKernel) {
+      output = this.biasKernel(output, inputs[2].data, this.oColSize) as number[];
+    }
+    outputs[0].setData(output);
+  }
+}
---
+++ test/functions/affine.test.ts
@@ -0,0 +1,47 @@
+import { AffineParameter } from '../../src/nnabla_pb';
+import Affine from '../../src/functions/affine';
+import Variable from '../../src/variable';
+import { expectClose } from '../testUtils';
+
+function affineRef(
+  x: number[],
+  w: number[],
+  b: number[],
+  xShape: number[],
+  wShape: number[],
+): number[] {
+  const [xRowSize, xColSize] = xShape;
+  const [, wColSize] = wShape;
+  const output = [...Array(xRowSize * wColSize)].map(() => 0.0);
+  for (let i = 0; i < xRowSize; i += 1) {
+    for (let j = 0; j < wColSize; j += 1) {
+      for (let k = 0; k < xColSize; k += 1) {
+        output[i * wColSize + j] += x[i * xColSize + k] * w[k * wColSize + j];
+      }
+    }
+  }
+  for (let i = 0; i < xRowSize; i += 1) {
+    for (let j = 0; j < wColSize; j += 1) {
+      output[i * wColSize + j] += b[j];
+    }
+  }
+  return output;
+}
+
+test('test-affine', () => {
+  const x = Variable.rand('x', [128, 64]);
+  const w = Variable.rand('w', [64, 32]);
+  const b = Variable.rand('b', [32]);
+  const y = Variable.rand('y', [128, 32]);
+  const param = AffineParameter.create();
+  param.baseAxis = 1;
+  const affine = new Affine(param);
+
+  affine.setup([x, w, b], [y]);
+  affine.forward([x, w, b], [y]);
+
+  const yRef = affineRef(x.data, w.data, b.data, x.shape, w.shape);
+  for (let i = 0; i < yRef.length; i += 1) {
+    expectClose(y.data[i], yRef[i], 0.000001);
+  }
+});
