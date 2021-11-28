6a4a4d48471d1087fd3aa1d0226277740403cedf
s-takuseno <Takuma.Seno@sony.com>
2021-11-28 18:37:48 +0900

    Add webpack build

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.ts', '.js',
+    ],
+  },
+};
