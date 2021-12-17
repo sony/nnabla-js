a6affc0cf3e6eb99af57569eba8e3d56d644f5e6
s-takuseno <Takuma.Seno@sony.com>
2021-12-17 14:41:45 +0900

    Update script to generate imagenet models

--- scripts/create_imagenet_nnp.py
+++ scripts/create_imagenet_nnp.py
@@ -1,10 +1,21 @@
+import argparse
 import nnabla as nn
-from nnabla.models.imagenet import ResNet18
+from nnabla.models.imagenet import ResNet18, MobileNetV2
 from nnabla.utils.save import save
 
 
 def main():
-    model = ResNet18()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--arch', type=str, default="mobilenetv2")
+    args = parser.parse_args()
+
+    if args.arch == "resnet":
+        model = ResNet18()
+    elif args.arch == "mobilenetv2":
+        model = MobileNetV2()
+    else:
+        raise ValueError(f"invalid architecture type: {args.arch}")
+
     x = nn.Variable((1,) + model.input_shape)
     y = model(x, training=False)
 
@@ -23,7 +34,7 @@ def main():
     }
 
     # save as nnp
-    save("resnet.nnp", contents)
+    save("imagenet.nnp", contents)
 
 
 if __name__ == "__main__":
